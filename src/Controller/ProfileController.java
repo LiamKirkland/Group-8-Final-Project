1148fabaec470203eb64cd9e933d941dda654940
Liam Kirkland <liam.b.kirkland@gmail.com>
2020-12-05 15:14:32 -0500

    Profile View now auto loads ID 1 (will change to selected ID when login is implemented)

--- src/Controller/ProfileController.java
+++ src/Controller/ProfileController.java
@@ -5,8 +5,12 @@
  */
 package Controller;
 
+import Model.Bio;
+import Model.Mainuser;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,8 +20,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 
 public class ProfileController implements Initializable {
@@ -33,13 +42,10 @@ public class ProfileController implements Initializable {
     @FXML
     private TextArea bioField;
     @FXML
+    private ImageView imageField;
+    @FXML
     private Button editProf;
 
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        
-    }   
     
     @FXML
     void openEditor(ActionEvent event) throws IOException {
@@ -54,5 +60,37 @@ public class ProfileController implements Initializable {
             stage.setScene(profileScene);
             stage.show();
     }
+    
+    EntityManager manager;
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        manager = (EntityManager) Persistence.createEntityManagerFactory("Group_8_Final_ProjectPU").createEntityManager();
+        
+        Query query = manager.createNamedQuery("Mainuser.findById");
+        query.setParameter("id", 1);
+        List<Mainuser> userList = query.getResultList();
+        Mainuser mainUser = userList.get(0);
+        Query bioQuery = manager.createNamedQuery("Bio.findById");
+        bioQuery.setParameter("id", 1);
+        List<Bio> bioList = bioQuery.getResultList();
+        Bio mainBio = bioList.get(0);
+        
+        
+        nameField.setText(mainUser.getName());
+        ageField.setText(String.valueOf(mainUser.getAge()));
+        yearField.setText(mainUser.getStudentyear());
+        majorField.setText(mainUser.getMajor());
+        bioField.setText(mainBio.getContents());
+        String imageName;
+        if(mainUser.getProfpic() != null){
+            imageName = "/resources/images/" + mainUser.getProfpic();
+        }else{
+            imageName = "/resources/images/default.jpg";
+        }
+        Image pfp = new Image(getClass().getResourceAsStream(imageName));
+        imageField.setImage(pfp);
+    }   
+    
+    
     
 }
